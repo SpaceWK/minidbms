5c90a2094cbb9ea0f9eefc65ea8aac13cc933709
Alex <58958500+SpaceWK@users.noreply.github.com>
2023-01-11 14:24:47 +0200

    Revert "Revert "Final implementation of Join algorithms. Sort merge Join and Index Nesteed Loop Join.""
    This reverts commit 5912d9f510213d9b3b2ff6ceb73ad5e85e8eefbf.

--- Client/Client/QUERIES.sql
+++ Client/Client/QUERIES.sql
@@ -9,11 +9,16 @@ INSERT INTO disciplines (DiscID, DName, CreditNr) VALUES ('DB1', 'Databases 1', 
 INSERT INTO disciplines (DiscID, DName, CreditNr) VALUES ('DS', 'Data Structures', 6);
 INSERT INTO disciplines (DiscID, DName, CreditNr) VALUES ('CP', 'C Programming', 6);
 
+INSERT INTO disciplines (DiscID, DName, CreditNr) VALUES ('C', 'Calculator', 6);
+
 
 # Specialization
 CREATE TABLE specialization (SpecID VARCHAR(5) PRIMARY KEY, SpecName VARCHAR(40), Language VARCHAR(20));
 
 INSERT INTO specialization (SpecID, SpecName, Language) VALUES ('I', 'Informatics', 'English');
+INSERT INTO specialization (SpecID, SpecName, Language) VALUES ('MI', 'Maths', 'English');
+INSERT INTO specialization (SpecID, SpecName, Language) VALUES ('S', 'Sport', 'Deutch');
+INSERT INTO specialization (SpecID, SpecName, Language) VALUES ('MJ', 'Michael Jackson', 'English');
 
 
 # Groups
@@ -25,7 +30,7 @@ INSERT INTO groups (GroupID, SpecID) VALUES (631, 'MI'); # MI does not exist in 
 
 
 # Students
-CREATE TABLE students (StudID INT PRIMARY KEY, GroupID INT REFERENCES groups (GroupID));
+CREATE TABLE students (StudID INT PRIMARY KEY, GroupID INT REFERENCES groups (GroupID), StudName VARCHAR(40), Email VARCHAR(40));
 
 INSERT INTO students (StudID, GroupID, StudName, Email) VALUES (1, 531, 'John Foreman', 'JohnForeman@email.com');
 INSERT INTO students (StudID, GroupID, StudName, Email) VALUES (2, 531, 'Ashley Cole', 'AshleyCole@email.com');
@@ -38,10 +43,15 @@ CREATE TABLE marks (StudID INT(10) REFERENCES students (StudID), DiscID VARCHAR(
 
 INSERT INTO marks (StudID, DiscID, Mark) values (1, 'DB1', 10);
 INSERT INTO marks (StudID, DiscID, Mark) values (1, 'DS', 9);
+INSERT INTO marks (StudID, DiscID, Mark) values (2, 'DB1', 8);
+INSERT INTO marks (StudID, DiscID, Mark) values (2, 'CP', 9);
+INSERT INTO marks (StudID, DiscID, Mark) values (3, 'DS', 5);
+INSERT INTO marks (StudID, DiscID, Mark) values (1, 'CP', 7);
 
 
 # Index
 CREATE INDEX idx_marks_Mark ON marks (Mark);
+CREATE INDEX idx_disciplines_CreditNr ON disciplines (CreditNr);
 
 INSERT INTO marks (StudID, DiscID, Mark) values (1, 'CP', 10); # Should update marks IDX collections
 
@@ -61,6 +71,8 @@ SELECT CreditNr FROM disciplines WHERE CreditNr > 1 AND DName = 'Databases 1';
 
 SELECT Mark FROM marks WHERE DiscID = 'DB1';
 
+SELECT students.StudID, students.StudName, marks.DiscID, marks.Mark FROM students INNER JOIN marks ON students.StudID = marks.StudID;
+SELECT specialization.SpecID, groups.GroupID, specialization.SpecName, specialization.Language FROM specialization INNER JOIN groups ON specialization.SpecID = groups.SpecID;
 
 DROP TABLE marks;
 DROP DATABASE db;
